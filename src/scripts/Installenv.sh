3150feb759c53921204fbc05ba28a15a1737d0db
Rafael Paes Leme <11760799+rafaelspaesleme-ads@users.noreply.github.com>
2019-05-22 00:01:58 -0300

    Update Installenv.sh

--- src/scripts/Installenv.sh
+++ src/scripts/Installenv.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Criando pasta principalcc
-mkdir "./DevOps"
+mkdir "./$JENKINS_HOME/DevOps"
 
 # Criando subpastas
-mkdir "./DevOps/projects"
-mkdir "./DevOps/lib"
-mkdir "./DevOps/trash"
-mkdir "./DevOps/docs"
-mkdir "./DevOps/config"
+mkdir "./$JENKINS_HOME/DevOps/projects"
+mkdir "./$JENKINS_HOME/DevOps/lib"
+mkdir "./$JENKINS_HOME/DevOps/trash"
+mkdir "./$JENKINS_HOME/DevOps/docs"
+mkdir "./$JENKINS_HOME/DevOps/config"
 
 # Criando pasta de repositorio de projetos
-mkdir "./DevOps/projects/repositories"
-mkdir "./DevOps/projects/trash"
-mkdir "./DevOps/projects/docs"
+mkdir "./$JENKINS_HOME/DevOps/projects/repositories"
+mkdir "./$JENKINS_HOME/DevOps/projects/trash"
+mkdir "./$JENKINS_HOME/DevOps/projects/docs"
 
 # Criando workspace
 PROJECT_BREAK_DOWN=( `echo ${NOME_PROJECTS} | sed -e 's/[,\/]/ /g'` )
@@ -22,11 +22,11 @@ echo $PROJECT_BREAK_DOWN
 
 for ((i=0; i<${#PROJECT_BREAK_DOWN[@]}; ++i))
 do
-   if [ -e "./DevOps/projects/repositories/${PROJECT_BREAK_DOWN[(($i))]}" ]
+   if [ -e "./$JENKINS_HOME/DevOps/projects/repositories/${PROJECT_BREAK_DOWN[(($i))]}" ]
    then
       echo "O diretorio ${PROJECT_BREAK_DOWN[(($i))]} existe"
    else
       echo "Criando repositório ${PROJECT_BREAK_DOWN[(($i))]}"
-      mkdir "./DevOps/projects/repositories/${PROJECT_BREAK_DOWN[(($i))]}"
+      mkdir "./$JENKINS_HOME/DevOps/projects/repositories/${PROJECT_BREAK_DOWN[(($i))]}"
    fi
 done
