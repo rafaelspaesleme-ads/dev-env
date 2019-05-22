439a0685f72c1a6918964589e72ac18d71f17caf
Rafael Paes Leme <11760799+rafaelspaesleme-ads@users.noreply.github.com>
2019-05-22 01:46:20 -0300

    Update Gkey.sh

--- src/scripts/Gkey.sh
+++ src/scripts/Gkey.sh
@@ -12,4 +12,4 @@ ssh-keygen -t rsa -N "" -f ~/.ssh/$CREATE_PKEY
 
 sshpass -p $PASSWORD_SERVER ssh-copy-id -i ~/.ssh/$CREATE_PKEY.pub $USER_SERVER@$HOST_SERVER
 
-ssh $USER_SERVER@$HOST_SERVER -i ~/.ssh/CREATE_PKEY.pub
+ssh $USER_SERVER@$HOST_SERVER
