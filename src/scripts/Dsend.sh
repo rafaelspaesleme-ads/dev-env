50da167a3d90c0d994cb8c65cc1db5be28e41d7b
Rafael Paes Leme <11760799+rafaelspaesleme-ads@users.noreply.github.com>
2019-05-22 01:09:34 -0300

    Update Dsend.sh

--- src/scripts/Dsend.sh
+++ src/scripts/Dsend.sh
@@ -2,4 +2,4 @@
 
 chmod -R 777 DevOps/
 
-scp -i $JENKINS_HOME/.ssh/$CREATE_PKEY.pub DevOps/ $USER_SERVER@$HOST_SERVER:/home/$USER_SERVER
+scp -i $JENKINS_HOME/.ssh/$CREATE_PKEY.pub -r DevOps/ $USER_SERVER@$HOST_SERVER:/home/$USER_SERVER
