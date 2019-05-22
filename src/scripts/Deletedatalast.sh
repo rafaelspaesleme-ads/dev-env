ca03caf79772d291e6c0dfdaa7986f77210d5604
Rafael Paes Leme <11760799+rafaelspaesleme-ads@users.noreply.github.com>
2019-05-21 23:58:59 -0300

    Update Deletedatalast.sh

--- src/scripts/Deletedatalast.sh
+++ src/scripts/Deletedatalast.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ -e "./DevOps" ]
+if [ -e "./$JENKINS_HOME/DevOps" ]
    then
       echo "Excluindo repositorio antigo DevOps"
-      rm -R ./DevOps
+      rm -R ./$JENKINS_HOME/DevOps
    else
       echo "O diretorio DevOps nao existe"
 fi
