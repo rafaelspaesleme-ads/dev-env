4439ba4f198175ccc965e74934ec49221a21ec68
Rafael Paes Leme <11760799+rafaelspaesleme-ads@users.noreply.github.com>
2019-05-22 01:02:19 -0300

    Update Gkey.sh

--- src/scripts/Gkey.sh
+++ src/scripts/Gkey.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Gerando chave ssh
-if [ -e "/$JENKINS_HOME/.ssh" ]
+if [ -e "$JENKINS_HOME/.ssh" ]
    then
       echo "pasta .ssh ja existe!"
    else
-      mkdir /$JENKINS_HOME/.ssh
+      mkdir $JENKINS_HOME/.ssh
 fi
 
-ssh-keygen -t rsa -N "" -f /$JENKINS_HOME/.ssh/$CREATE_PKEY
+ssh-keygen -t rsa -N "" -f $JENKINS_HOME/.ssh/$CREATE_PKEY
 
-sshpass -p $PASSWORD_SERVER ssh-copy-id -i /$JENKINS_HOME/.ssh/$CREATE_PKEY.pub $USER_SERVER@$HOST_SERVER
+sshpass -p $PASSWORD_SERVER ssh-copy-id -i $JENKINS_HOME/.ssh/$CREATE_PKEY.pub $USER_SERVER@$HOST_SERVER
