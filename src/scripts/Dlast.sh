3e1d23942a08048714de062c69fa018fb5d393ba
Rafael Paes Leme <11760799+rafaelspaesleme-ads@users.noreply.github.com>
2019-05-22 00:26:48 -0300

    Update Dlast.sh

--- src/scripts/Dlast.sh
+++ src/scripts/Dlast.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -e "./$JENKINS_HOME/DevOps" ]
-   then
-      echo "Excluindo repositorio antigo DevOps"
-      rm -R ./$JENKINS_HOME/DevOps
-   else
-      echo "O diretorio DevOps nao existe"
+then
+echo "Excluindo repositorio antigo DevOps"
+rm -R ./$JENKINS_HOME/DevOps
+else
+echo "O diretorio DevOps nao existe"
 fi
