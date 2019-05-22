d36171c52df7b20abed4e4f4b4d62947b4cdf12e
Rafael Paes Leme <11760799+rafaelspaesleme-ads@users.noreply.github.com>
2019-05-22 00:00:00 -0300

    Update Datasend.sh

--- src/scripts/Datasend.sh
+++ src/scripts/Datasend.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-chmod -R 777 DevOps/
+chmod -R 777 /$JENKINS_HOME/DevOps/
+
+cd /$JENKINS_HOME
 
 scp -i /$JENKINS_HOME/.ssh/$CREATE_PKEY.pub DevOps/ $USER_SERVER@$HOST_SERVER:/home/$USER_SERVER
