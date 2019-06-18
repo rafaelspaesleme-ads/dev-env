6b07c2f2fafed6b0bde394d92a51252a5aead8b6
Rafael Paes Leme <11760799+rafaelspaesleme-ads@users.noreply.github.com>
2019-06-17 23:52:23 -0300

    Update Gkey.sh

--- src/scripts/Gkey.sh
+++ src/scripts/Gkey.sh
@@ -10,4 +10,4 @@ fi
 
 ssh-keygen -t rsa -N "" -f ~/.ssh/$CREATE_PKEY
 
-sshpass -p $PASSWORD_SERVER cat ~/.ssh/$CREATE_PKEY.pub | ssh $USER_SERVER@$HOST_SERVER 'cat >> .ssh/authorized_keys && echo "Chave copiada."'
+cat ~/.ssh/$CREATE_PKEY.pub | ssh $USER_SERVER@$HOST_SERVER 'cat >> .ssh/authorized_keys && echo "Chave copiada."'
